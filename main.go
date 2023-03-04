08a583f9b03d4a87c79f292e2fdddd473feca46a
Tony Zhang <toniczh@gmail.com>
2023-03-04 16:38:06 -0500

    add models

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 	router.GET("/:name", indexHandler)
+	router.GET("/recipes", getAllRecipes)
 	router.Run()
 }
 
 func indexHandler(ctx *gin.Context) {
 	name := ctx.Params.ByName("name")
 	ctx.JSON(200, gin.H{"message": name})
+}
+
+func getAllRecipes(ctx *gin.Context) {
+	ctx.JSON(200, recipes)
 }
